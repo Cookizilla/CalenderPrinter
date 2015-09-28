d300de9cef5957923b4f43a133537029942ee8e0
cookizilla <cookizilla@gmail.com>
2015-09-28 18:21:30 +0000

    This is your commit message.

---
+++ CalendarPrinter.java
@@ -0,0 +1,21 @@
+public class CalendarPrinter {
+  public static void main (String[] args) {
+        int x = 1;
+        String n = "";
+        while (x < 31){
+            if (x == 5 || x == 6 || x == 12 || x == 13 || x == 19 || x == 20 || x == 26 || x == 27){
+                n = " - WEEKEND";
+            };
+            if (x == 7){
+                n = " - NO SCHOOL (LABOR DAY";
+            };
+            if (x == 25){
+                n = " - END OF THE MARKING PERIOD";
+            };
+            System.out.println("September " + x + n);
+            n = "";
+            x++;
+        }
+    }
+}
+
